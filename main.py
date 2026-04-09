4bb430dbee89fbbcba7fcb0ad329ead927563413
chernorusic <chernorusic@gmail.com>
2026-04-09 18:22:54 +0300

    Пофиксил баг кнопок, обновил функцию выбора новеллы и добавил функцию отправки сообщений

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import Invasion
 import Liberator
 import Mystical_Hotel
 
-Kvestbot = telebot.TeleBot('8752020452:AAFefZcxitQDYn9fqaOq0_86h4uKZd4o-QA')
+Kvestbot = telebot.TeleBot('8355491412:AAEOfsPuvGZxZpSTFP6Gy1AhUBJJvn_gK1Q')
 
 Start_Menu = '''Добро пожаловать в сборник моих новелл.
 
@@ -52,6 +52,7 @@ Admin_id = 882977571
 users_option_novella = {}
 users_step = {}
 users_check = {}
+bot_message = {}
 
 keyboard = types.InlineKeyboardMarkup()
 
@@ -64,122 +65,103 @@ def options(chat_data, option): # Выбор варианта
         option = 2
     return option
 
-def option_novellas(chat_data, option_novella): # Выбранная новелла
+def option_novellas(chat_data, user_id): # Выбранная новелла
     if chat_data == 'Mystical_Hotel':
-        option_novella = 0
+        users_step[user_id] = "0"
+        users_option_novella[user_id] = Mystical_Hotel.Mystical_Hotel
     elif chat_data == 'Invasion':
-        option_novella = 1
+        users_step[user_id] = "0"
+        users_option_novella[user_id] = Invasion.Invasion
     elif chat_data == 'Liberator':
-        option_novella = 25
-    return option_novella
+        users_step[user_id] = "0"
+        users_option_novella[user_id] = Liberator.Liberator
 
 def buttons(buttons, keyboard, btn1, btn2, btn3): # Ввод клавы
     if buttons == 1:
-        keyboard.keyboard.clear()
         keyboard.add(btn1)
     elif buttons == 2:
-        keyboard.keyboard.clear()
         keyboard.add(btn1, btn2)
     elif buttons == 3:
-        keyboard.keyboard.clear()
         keyboard.add(btn1, btn2, btn3)
-    elif buttons == 0:
+
+def sending_message(chat, chat_id, Text, keyboard = None):
+    user_id = chat.from_user.id
+    if user_id in bot_message:
+        try:
+            Kvestbot.edit_message_reply_markup( # Удаляем кнопки у всех предыдущих сообщений
+                chat_id=chat_id,
+                message_id=bot_message[user_id],
+                reply_markup=None
+            )
+        except:
+            pass
+    if keyboard != None:
+        message = Kvestbot.send_message(chat.from_user.id, Text, reply_markup = keyboard)
         keyboard.keyboard.clear()
+    else:
+        message = Kvestbot.send_message(chat.from_user.id, Text)
+    Kvestbot.send_message(Admin_id, f'Пользователь {chat.from_user.first_name} {chat.from_user.last_name} '
+                          f'({chat.from_user.username}, {chat.from_user.language_code}) Получил сообщение: \n '
+                          f'\n {Text}')
+    bot_message[user_id] = message.message_id
 
 @Kvestbot.message_handler(commands = ["start"])
 def start(chat):
-    user_id = chat.from_user.id
-    users_step[user_id] = "0"
-    users_option_novella[user_id] = None
-    keyboard.keyboard.clear()
     keyboard.add(btn_Mystical_Hotel, btn_Invasion)
-    Kvestbot.send_message(chat.from_user.id, Start_Menu, reply_markup=keyboard)
+    sending_message(chat, chat.chat.id, Start_Menu, keyboard)
+
+@Kvestbot.message_handler(commands = ["menu"])
+def Menu(chat):
+    keyboard.add(btn_Mystical_Hotel, btn_Invasion)
+    sending_message(chat, chat.chat.id, Start_Menu, keyboard)
+
+@Kvestbot.message_handler(commands = ["stop"])
+def stop(chat):
+    keyboard.add(btn_Mystical_Hotel, btn_Invasion)
+    sending_message(chat, chat.chat.id, Stop_Menu, keyboard)
 
 @Kvestbot.message_handler(func = lambda chat: chat.text == "моя мать шлюха твоя мать спидозная сука")
 def sicret1(chat):
-    keyboard.keyboard.clear()
     keyboard.add(btn_cocal1, btn_cocal2)
-    Kvestbot.send_message(chat.from_user.id, Check1, reply_markup=keyboard)
+    sending_message(chat, chat.chat.id, Check1, keyboard)
 
 @Kvestbot.message_handler(func = lambda chat: chat.text == "сто жирных армянских хуев тебе в жопу")
 def sicret2(chat):
     user_id = chat.from_user.id
-    keyboard.keyboard.clear()
     if user_id not in users_check: # Если чел не прошел 1 этап
         users_check[user_id] = False
-    users_step[user_id] = "0"
-    users_option_novella[user_id] = None
     if users_check[user_id] == True: # Проверка прошел ли 1 этап
         keyboard.add(btn_Liberator)
-        Kvestbot.send_message(chat.from_user.id, Check2, reply_markup=keyboard)
+        sending_message(chat, chat.chat.id, Check2, keyboard)
     else:
-        keyboard.add(btn_cocal1, btn_cocal2)
-        Kvestbot.send_message(chat.from_user.id, "Ты ввел какую то хуйню, иди нахуй", reply_markup=keyboard)
-
-@Kvestbot.message_handler(commands = ["menu"])
-def Menu(chat):
-    user_id = chat.from_user.id
-    users_step[user_id] = "0"
-    users_option_novella[user_id] = None
-    keyboard.keyboard.clear()
-    keyboard.add(btn_Mystical_Hotel, btn_Invasion)
-    Kvestbot.send_message(chat.from_user.id, Start_Menu, reply_markup=keyboard)
-    Kvestbot.send_message(Admin_id, f'Пользователь {chat.from_user.first_name} {chat.from_user.last_name} ({chat.from_user.username}, {chat.from_user.language_code}) Получил сообщение: \n \n {Start_Menu}')
-
-@Kvestbot.message_handler(commands = ["stop"])
-def stop(chat):
-    user_id = chat.from_user.id
-    users_step[user_id] = "0"
-    users_option_novella[user_id] = None
-    keyboard.keyboard.clear()
-    keyboard.add(btn_Mystical_Hotel, btn_Invasion)
-    Kvestbot.send_message(chat.from_user.id, Stop_Menu, reply_markup=keyboard)
-
+        sending_message(chat, chat.chat.id, "Ты ввел какую то хуйню, иди нахуй")
 
 @Kvestbot.callback_query_handler(func=lambda call: call.data == "cocal")
 def Cocal(chat):
     user_id = chat.from_user.id
-    Kvestbot.edit_message_reply_markup( # Удаляем кнопки у всех предыдущих сообщений
-        chat_id=chat.message.chat.id,
-        message_id=chat.message.message_id,
-        reply_markup=None
-    )
     users_check[user_id] = True
-    Kvestbot.send_message(chat.from_user.id, "АХАХАХАХАХААХ, ебать ты лошара, каким же надо быть "
+    sending_message(chat, chat.message.chat.id, "АХАХАХАХАХААХ, ебать ты лошара, каким же надо быть "
                                                          "долбаебом, чтобы кликнуть на такую хуйню?))")
 
 @Kvestbot.callback_query_handler(func=lambda call: True)
 def Cocal(chat):
     user_id = chat.from_user.id # Сбор данных и обьявление переменных
     option = None
-    option_novella = None
     chat_data = chat.data
-    users_option_novella[user_id] = option_novellas(chat_data, users_option_novella[user_id]) # Проверка выбора новеллы
-    if users_option_novella[user_id] == 0:
-        option_novella = Mystical_Hotel.Mystical_Hotel
-    elif users_option_novella[user_id] == 1: #Вот сюда надо добавить новую новеллу
-        option_novella = Invasion.Invasion
-    elif users_option_novella[user_id] == 25:
-        option_novella = Liberator.Liberator
+    option_novellas(chat_data, user_id) # Проверка выбора новеллы
+    option_novella = users_option_novella[user_id]
     option = options(chat_data, option) # Перевод шага на выбранный вариант
     if option != None:
         users_step[user_id] = option_novella[users_step[user_id]]["options"][option]
-    Kvestbot.edit_message_reply_markup( # Добавляем клаву
-        chat_id=chat.message.chat.id,
-        message_id=chat.message.message_id,
-        reply_markup=None
-    )
-    buttons_1 = option_novella[users_step[user_id]]["buttons"]
+    buttons_1 = option_novella[users_step[user_id]]["buttons"] #Добавляем кнопки
     buttons(buttons_1, keyboard, btn1, btn2, btn3)
-    Kvestbot.send_message(chat.from_user.id, option_novella[users_step[user_id]]["Text"], reply_markup=keyboard) #Вывод сообщения
+    sending_message(chat, chat.message.chat.id, option_novella[users_step[user_id]]["Text"], keyboard) #отправляем сообщение
     if buttons_1 == 0: # Вывод сообщения о конце новеллы
-        if option_novella != 25:
-            users_step[user_id] = "0"
+        if users_option_novella[user_id] != Liberator.Liberator: #Ебанутые новеллы добавлять сюда!!
             keyboard.add(btn_Mystical_Hotel, btn_Invasion)
-            Kvestbot.send_message(chat.from_user.id, End_Novella, reply_markup=keyboard)
+            sending_message(chat, chat.message.chat.id, End_Novella, keyboard)
         else:
             keyboard.add(btn_Liberator)
-            Kvestbot.send_message(chat.from_user.id, End_Check, reply_markup=keyboard)
-    print(users_step[user_id]) # Заменить на отправку сообщений мне
+            sending_message(chat, chat.message.chat.id, End_Check, keyboard)
 
 Kvestbot.polling(none_stop = True)
